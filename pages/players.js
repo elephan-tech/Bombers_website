a1fb35f387b0bc33c37eda41b794c6a51b8eed18
Victor <vict9210@gmail.com>
2021-03-20 14:59:21 -0500

    Empezamos pagina

---
+++ pages/players.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+const players = () => {
+    return (
+        <><div>
+            Players
+        </div>
+        
+        <Link href= './'><a>Home</a></Link></>
+    )
+}
+
+export default players
